199175b283a4c1e7a577b10374fefb989cda60a0
Hoon Lee <yl.hoony@gmail.com>
2017-10-02 19:06:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function printBadges(array) {
+  for (let i = 0; i < array.length; i++) {
+    // console.log(array[i]);
+    console.log(`Welcome ${array[i]}! You are employee #${[i + 1]}.`)
+  }
+  return array;
+}
+
+function tailsNeverFails() {
+  let tails = 0;
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`;
+}
