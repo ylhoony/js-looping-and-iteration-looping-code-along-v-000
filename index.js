6da9c8df092207251c41385c3e85afd3d8a06b96
Hoon Lee <yl.hoony@gmail.com>
2017-10-02 19:05:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function printBadges(array) {
 
 function tailsNeverFails() {
   let tails = 0;
-  while (Math.random >= 0.5) {
+  while (Math.random() >= 0.5) {
     tails++;
   }
   return `You got ${tails} tails in a row!`;
