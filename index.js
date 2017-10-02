184aec0748fd5dcf38aac1d0679239341995eb5c
Hoon Lee <yl.hoony@gmail.com>
2017-10-02 18:53:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 
 function printBadges(array) {
-  for (i = 0; i < array.length -1; i++) {
+  for (let i = 0; i < array.length -1; i++) {
     console.log(array[i]);
   }
 }
