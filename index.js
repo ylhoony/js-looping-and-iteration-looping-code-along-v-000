f8ce0cde6faafb65a8a3c395acf301ab9dee39f4
Hoon Lee <yl.hoony@gmail.com>
2017-10-02 18:55:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function printBadges(array) {
   for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
+    // console.log(array[i]);
     console.log(`Welcome ${array[i]}! You are employee #${[i + 1]}.`)
   }
 }
